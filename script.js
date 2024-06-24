d88ff65b1df04ec66f09a82f750d23dd192ce9de
DiscoStrangler69 <ijazarslan372@gmail.com>
2024-06-24 14:40:56 +0500

    Added the functionality to change grid size

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const container = document.getElementById('container');
 const resetButton = document.getElementById('resetButton');
-function createGrid(size) {
+const sizeButton = document.getElementById('numSquares');
+
+sizeButton.addEventListener('click',function(){
+    let squares = prompt("Enter the number of squares in a row:");
+    container.innerHTML = '';
+    createGrid(squares);
+
+
+})
+function createGrid(size=16) {
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.style.width = 480 / size + 'px';
+        square.style.height = 480 / size + 'px';
         square.addEventListener('mouseover', changeColor);
         container.appendChild(square);
 
